a2de101993828d479451fc17e404d43ef004f47d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-19 20:57:47 +0530

    added the slices - increment decrement

---
+++ src/redux/counter/index.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  initialState: 0,
+  name: "counter",
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+});
