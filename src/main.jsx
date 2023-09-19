aa547638f60a0b2c7034b2eee2e845f9a9f62bd9
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-19 20:55:17 +0530

    access of store given to the app component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      {/* by passing here the store , the app component will have access to the whole store. */}
+      <App />
+    </Provider>
+    ,
+  </React.StrictMode>
+);
